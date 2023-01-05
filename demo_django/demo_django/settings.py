85bfa5494f7bf244667e4fc77aee0e4e1fa13f1b
Thong <petomo2003@gmail.com>
2023-01-05 14:00:43 +0700

    add staticfiles

--- demo_django/demo_django/settings.py
+++ demo_django/demo_django/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%z2xpad9^d109fggux4-0n3op3iin+!%nud0iha26&&y4qo!*i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -128,8 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT='%s/app_demo_grapql/' %BASE_DIR
 CKEDITOR_UPLOAD_PATH = 'static/content/ckeditor/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
