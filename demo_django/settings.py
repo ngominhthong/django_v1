e125f649a14c44365711575917c5364de6b573d8
Thong <petomo2003@gmail.com>
2023-01-05 14:43:56 +0700

    STATIC_URL true

--- demo_django/settings.py
+++ demo_django/settings.py
@@ -137,7 +137,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_URL = True
 STATIC_URL = 'static/'
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT='%s/app_demo_grapql/' %BASE_DIR
